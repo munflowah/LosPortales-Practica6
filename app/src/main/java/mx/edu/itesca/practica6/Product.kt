a7d6a3222060f637d87b00e89e25062651aea9cf
munflowah <fshlm@hotmail.com>
2024-02-14 16:27:29 -0700

    avance antes de clase

---
+++ app/src/main/java/mx/edu/itesca/practica6/Product.kt
@@ -0,0 +1,6 @@
+package mx.edu.itesca.practica6
+
+class Product ( var name:String,
+                var image: Int,
+                var description:String,
+                var price:Double  )
---
+++ app/src/main/java/mx/edu/itesca/practica6/ProductosActivity.kt
@@ -0,0 +1,73 @@
+package mx.edu.itesca.practica6
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+
+class ProductosActivity : AppCompatActivity() {
+    var menu: ArrayList<Product> = ArrayList<Product>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos)
+        agregarProductos()
+
+
+        var listview: ListView = findViewById(R.id.listview) as ListView
+
+        var adaptador: AdaptadorProductos = AdaptadorProductos ( this, menu)
+        listview.adapter = adaptador
+    }
+        fun agregarProductos(){
+            menu.add(Product( "Quesadillas", R.drawable.quesadillas, "Rellenas con su carne favorita, servidas con ensalada. Filled with your choice of meat, servet with salad. ",5.99))
+            menu.add(Product( "Huaraches", R.drawable.huaraches,  "Tortilla gruesa con frijoles, tu carne favorita, lechuga, queso fresco y crema - Big thick tortilla with beans, your choice of meat, fresh cheese and sour cream ", 10.99))
+            menu.add(Product( "Gringas", R.drawable.gringas, "Tortilla de harina con queso, carne al pastor y piña Flour tortilla filled with cheese, marinated pork and pineapple",7.99))
+            menu.add(Product( "Sincronizadas", R.drawable.sincronizadas,  "Tortilla de harina rellena con queso y jamón. Acompañada de lechuga, crema y guacamole - Two four tortillas filled with ham and cheese. Served with lettuce, sour cream, and guacamole. ",7.99))
+            menu.add(Product( "Sopes", R.drawable.sopes,  "Tortilla gruesa cubierta de frijoles, tu carne favorita, lechuga, queso fresco y crema - Fried thick tortilla with beans, your choice of meat, lettuce, fresh cheese, and sour cream. ",3.79))
+            menu.add(Product( "Tostadas", R.drawable.tostadas, "Tortilla frita con frijoles, tu carne favorita, lechuga, queso fresco, crema y jitomate - Fried tortilla with beans, your choice of meat, lettuce, fresh cheese, sour cream and tomatoes.",4.59))
+        }
+
+    private class AdaptadorProductos: BaseAdapter {
+        var productos=ArrayList<Product>()
+        var contexto: Context?=null
+        constructor(contexto: Context, producto: ArrayList<Product>){
+            this.productos=productos
+            this.contexto=contexto
+        }
+
+        override fun getCount(): Int {
+            return productos.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return productos[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var prod=productos[p0]
+            var inflador= LayoutInflater.from(contexto)
+            var vista=inflador.inflate(R.layout.producto_view, null)
+
+            var imagen=vista.findViewById(R.id.producto_img) as ImageView
+            var nombre=vista.findViewById(R.id.producto_nombre) as TextView
+            var desc=vista.findViewById(R.id.producto_desc) as TextView
+            var precio=vista.findViewById(R.id.producto_precio) as TextView
+
+            imagen.setImageResource(prod.image)
+            nombre.setText(prod.name)
+            desc.setText(prod.description)
+            precio.setText("$${prod.price}")
+            return vista
+        }
+    }
+}
