2b2c0c7c9ad56f7902cafa3c386f67291d08a96f
Flor Margarita Soto Higuera <101524283+munflowah@users.noreply.github.com>
2024-02-15 23:32:09 -0700

    terminado de menu

--- app/src/main/java/mx/edu/itesca/practica6/MenuActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica6/MenuActivity.kt
@@ -11,16 +11,53 @@ class MenuActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        var btnAntojitos: Button = findViewById(R.id.button_antojitos) as Button
-        var btnEspecialidades: Button = findViewById(R.id.button_especialidades) as Button
-        var btnCombinations: Button = findViewById(R.id.button_combinations) as Button
-        var btnTortas: Button = findViewById(R.id.button_tortas) as Button
-        var btnSopas: Button = findViewById(R.id.button_sopas) as Button
-        var btnDrinks: Button = findViewById(R.id.button_drinks) as Button
+        var btnAntojitos: Button = findViewById<Button>(R.id.button_antojitos)
+        var btnEspecialidades: Button = findViewById<Button>(R.id.button_especialidades)
+        var btnCombinations: Button = findViewById<Button>(R.id.button_combinations)
+        var btnTortas: Button = findViewById<Button>(R.id.button_tortas)
+        var btnSopas: Button = findViewById<Button>(R.id.button_sopas)
+        var btnDrinks: Button = findViewById<Button>(R.id.button_drinks)
 
 
         btnAntojitos.setOnClickListener{
             var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("MenuType", "Antojitos")
+            intent.putExtra("drawableId", R.drawable.antojitos)
+            startActivity(intent)
+        }
+
+        btnEspecialidades.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("MenuType", "Especialidades")
+            intent.putExtra("drawableId", R.drawable.especialidades)
+            startActivity(intent)
+        }
+
+        btnCombinations.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("MenuType", "Combinations")
+            intent.putExtra("drawableId", R.drawable.combinations)
+            startActivity(intent)
+        }
+
+        btnTortas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("MenuType", "Tortas")
+            intent.putExtra("drawableId", R.drawable.tortas)
+            startActivity(intent)
+        }
+
+        btnSopas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("drawableId", R.drawable.sopas)
+            intent.putExtra("MenuType", "Sopas")
+            startActivity(intent)
+        }
+
+        btnDrinks.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("drawableId", R.drawable.drinks)
+            intent.putExtra("MenuType", "Drinks")
             startActivity(intent)
         }
     }
